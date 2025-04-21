95f83288a655ef447bff1a1d9edab8ded525bf3f
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-04-21 12:49:03 +0300

    Improve smooth block transition code
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 var sections = [ "home", "install", "docs", "faq", "links" ];
-
-function switchSection(sectionName) {
-    sections.forEach((section) => {
-        if (section != sectionName) {
-            document.getElementById(section + "-section").classList.add("hidden");
-        };
-    });
-    document.getElementById(sectionName + "-section").classList.remove("hidden");
-}
-
 var currentSection = sections[0];
+
+function switchSection(newSection) {
+    sections.forEach((section) => {
+        document.getElementById(section + "-section").classList.add("hidden");
+    });
+
+    document.getElementById(newSection + "-section").classList.remove("hidden");
+}
 
 if (document.baseURI.lastIndexOf("#") !== -1) {
     currentSection = document.baseURI.substr(document.baseURI.lastIndexOf("#") + 1).toLowerCase();
     if (document.getElementById(currentSection + "-section") === null) {
-        currentSection = sections[0];
+         currentSection = sections[0];
     };
 };
 
