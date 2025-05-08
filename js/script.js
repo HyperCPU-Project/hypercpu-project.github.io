c88961349f53a3d905a65c35e798ae184aaff461
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-05-08 14:33:39 +0300

    Invalid content scrolling fix and other improvements
    1. Finally, fixed an invalid content scrolling bug, when a visitor could scroll to the end of the biggest section block and see a big empty space if the block is hidden. Various CSS & JavaScript tweaks and tricks have been used. As a result, section switch animation changed a bit.
    2. Improved website appearance and behavior on mobile devices.
    3. Added dynamic page title change when switching between sections.
    4. Added documentation icon and links where possible.
    5. Other little fixes and improvements.
    
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
 var sections = document.getElementsByClassName("section");
-var currentSection = sections[0].id;
+var root = document.querySelector(":root");
+var transitionDuration = (parseFloat(getComputedStyle(root).getPropertyValue("--transition-duration")) * 1000);
 
 function onload() {
-    currentSection = sections[0].id;
-    console.log(document.baseURI);
-    
+    var currentSectionID = sections[0].id;
+
     if (document.baseURI.lastIndexOf("#") !== -1) {
-        currentSection = document.baseURI.substr(document.baseURI.lastIndexOf("#") + 1).toLowerCase() + "-section";
-        if (document.getElementById(currentSection) === null) {
-            currentSection = sections[0].id;
+        currentSectionID = document.baseURI.substring(document.baseURI.lastIndexOf("#") + 1).toLowerCase() + "-section";
+
+        if (document.getElementById(currentSectionID) === null) {
+            currentSectionID = sections[0].id;
         };
     };
 
-    for (var section in [].slice.call(sections)) {
-        var sectionID = sections[section].id;
-        document.getElementById(sectionID).classList.add("hidden");
-        document.getElementById(sectionID.replace("section", "button")).classList.remove("menu-button-active-section");
+    var currentSection = document.getElementById(currentSectionID);
+    var currentSectionName = currentSection.dataset.name;
+    var currentMenuButton = document.getElementById(currentSectionID.replace("section", "button"));
+
+    document.title = "HyperCPU Project" + ((currentSectionName.length !== 0) ? (": " + currentSectionName) : "");
+
+    currentSection.style.display = "inherit";
+    currentMenuButton.classList.add("menu-button-active-section");
+    currentMenuButton.focus();
+
+    for (var sectionString in [].slice.call(sections)) {
+        var sectionID = sections[sectionString].id;
+
+        if (sectionID === currentSectionID) {
+            continue;
+        };
+
+        var section = document.getElementById(sectionID);
+        var menuButton = document.getElementById(sectionID.replace("section", "button"));
+
+        section.classList.add("hidden");
+        
+        if (menuButton.classList.contains("menu-button-active-section")) {
+            menuButton.classList.remove("menu-button-active-section");
+        };
     };
 
-    document.getElementById(currentSection).classList.remove("hidden");
-    document.getElementById(currentSection.replace("section", "button")).classList.add("menu-button-active-section");
+    setTimeout(() => {
+        currentSection.classList.remove("hidden");
+
+        for (var sectionString in [].slice.call(sections)) {
+            var sectionID = sections[sectionString].id;
+            var section = document.getElementById(sectionID);
+
+            if (section.classList.contains("hidden")) {
+                section.style.display = "none";
+            };
+        };
+    }, transitionDuration);
 };
